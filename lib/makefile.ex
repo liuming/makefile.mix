698c734a3083db5d45baa80fe74643c92fd332bb
Ming Liu <liuming@lmws.net>
2017-02-16 14:46:43 -0800

    initial blank project

---
+++ lib/makefile.ex
@@ -0,0 +1,18 @@
+defmodule Makefile do
+  @moduledoc """
+  Documentation for Makefile.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Makefile.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/makefile_test.exs
@@ -0,0 +1,8 @@
+defmodule MakefileTest do
+  use ExUnit.Case
+  doctest Makefile
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
